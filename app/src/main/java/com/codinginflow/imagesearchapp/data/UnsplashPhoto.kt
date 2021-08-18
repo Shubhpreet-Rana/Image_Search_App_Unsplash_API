b120b260ca672fa6ceb0377422289f0ccfda0b99
Shubm <shubhpreet@euroinfotek.com>
2021-08-18 11:59:44 +0530

    Retrofit

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashPhoto.kt
@@ -0,0 +1,31 @@
+package com.codinginflow.imagesearchapp.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+    val id : String,
+    val descriptions : String?,
+    val urls : UnsplashPhotoUrls,
+    val user : UnsplashPhotoUsers
+): Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw : String,
+        val full : String,
+        val regular : String,
+        val small : String,
+        val thumb : String,
+    ):Parcelable
+
+    @Parcelize
+    data class UnsplashPhotoUsers(
+        val name : String,
+        val username : String
+    ):Parcelable{
+        val attributionUrls get() = "http//unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=refferal"
+    }
+
+}
